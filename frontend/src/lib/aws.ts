d1d528796500a935e6b3bb5fb37ae3f9f08381c1
Rikky J <rikkyj108@gmail.com>
2026-09-11 13:46:07 +0530

    fix(aws): specify default us-east-1 region for S3 clients

--- frontend/src/lib/aws.ts
+++ frontend/src/lib/aws.ts
@@ -5,6 +5,7 @@ function getS3Client(): S3 {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
     endpoint: process.env.S3_ENDPOINT,
+    region: process.env.AWS_REGION || "us-east-1",
     s3ForcePathStyle: true,
   });
 }
